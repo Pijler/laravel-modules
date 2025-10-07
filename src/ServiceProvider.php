ceefa79f773f966e68005d53bc7c9fa3420b3da2
João Pedro Lopes <joaopalopes24@gmail.com>
2025-10-07 17:57:57 -0300

    wip

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -10,6 +10,26 @@ use Modules\Commands\ComponentMakeCommand;
 use Modules\Commands\ConfigMakeCommand;
 use Modules\Commands\ConsoleMakeCommand;
 use Modules\Commands\ControllerMakeCommand;
+use Modules\Commands\EnumMakeCommand;
+use Modules\Commands\EventMakeCommand;
+use Modules\Commands\ExceptionMakeCommand;
+use Modules\Commands\InterfaceMakeCommand;
+use Modules\Commands\JobMakeCommand;
+use Modules\Commands\JobMiddlewareMakeCommand;
+use Modules\Commands\ListenerMakeCommand;
+use Modules\Commands\MailMakeCommand;
+use Modules\Commands\MiddlewareMakeCommand;
+use Modules\Commands\NotificationMakeCommand;
+use Modules\Commands\ObserverMakeCommand;
+use Modules\Commands\PolicyMakeCommand;
+use Modules\Commands\ProviderMakeCommand;
+use Modules\Commands\RequestMakeCommand;
+use Modules\Commands\ResourceMakeCommand;
+use Modules\Commands\RuleMakeCommand;
+use Modules\Commands\ScopeMakeCommand;
+use Modules\Commands\TestMakeCommand;
+use Modules\Commands\TraitMakeCommand;
+use Modules\Commands\ViewMakeCommand;
 use Modules\Support\Macros;
 
 class ServiceProvider extends LaravelServiceProvider
@@ -30,6 +50,26 @@ class ServiceProvider extends LaravelServiceProvider
                 ConfigMakeCommand::class,
                 ConsoleMakeCommand::class,
                 ControllerMakeCommand::class,
+                EnumMakeCommand::class,
+                EventMakeCommand::class,
+                ExceptionMakeCommand::class,
+                InterfaceMakeCommand::class,
+                JobMakeCommand::class,
+                JobMiddlewareMakeCommand::class,
+                ListenerMakeCommand::class,
+                MailMakeCommand::class,
+                MiddlewareMakeCommand::class,
+                NotificationMakeCommand::class,
+                ObserverMakeCommand::class,
+                PolicyMakeCommand::class,
+                ProviderMakeCommand::class,
+                RequestMakeCommand::class,
+                ResourceMakeCommand::class,
+                RuleMakeCommand::class,
+                ScopeMakeCommand::class,
+                TestMakeCommand::class,
+                TraitMakeCommand::class,
+                ViewMakeCommand::class,
             ]);
         }
     }
