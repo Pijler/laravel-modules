0707914aae3c2ef95a52a72031470489352781d6
João Pedro Lopes <joaopalopes24@gmail.com>
2025-10-09 09:24:07 -0300

    feat(commands): finish view make command

--- src/Commands/ViewMakeCommand.php
+++ src/Commands/ViewMakeCommand.php
@@ -4,6 +4,8 @@ namespace Modules\Commands;
 
 use Illuminate\Foundation\Console\ViewMakeCommand as BaseViewMakeCommand;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
+use Illuminate\Support\Stringable;
 use Modules\Traits\BaseCommands;
 use Symfony\Component\Console\Attribute\AsCommand;
 
@@ -22,42 +24,75 @@ class ViewMakeCommand extends BaseViewMakeCommand
      */
     protected $description = 'Create a new view in the specified module';
 
-    // /**
-    //  * Get the destination view path.
-    //  *
-    //  * @param  string  $name
-    //  * @return string
-    //  */
-    // protected function getPath($name)
-    // {
-    //     return $this->viewPath(
-    //         $this->getNameInput().'.'.$this->option('extension'),
-    //     );
-    // }
+    /**
+     * Get the destination view path.
+     */
+    protected function getPath($name): string
+    {
+        return $this->viewPath(
+            $this->getNameInput().'.'.$this->option('extension'),
+        );
+    }
 
-    // /**
-    //  * Create the matching test case if requested.
-    //  *
-    //  * @param  string  $path
-    //  */
-    // protected function handleTestCreation($path): bool
-    // {
-    //     if (! $this->option('test') && ! $this->option('pest') && ! $this->option('phpunit')) {
-    //         return false;
-    //     }
+    /**
+     * Get the destination test case path.
+     */
+    protected function getTestPath(): string
+    {
+        $module = $this->argument('module');
 
-    //     $contents = preg_replace(
-    //         ['/\{{ namespace \}}/', '/\{{ class \}}/', '/\{{ name \}}/'],
-    //         [$this->testNamespace(), $this->testClassName(), $this->testViewName()],
-    //         File::get($this->getTestStub()),
-    //     );
+        return module_path($module, Str::of($this->testClassFullyQualifiedName())
+            ->replaceFirst("Modules\\{$module}\\", '')
+            ->replace('\\', '/')
+            ->append('Test.php')
+            ->value());
+    }
 
-    //     File::ensureDirectoryExists(dirname($this->getTestPath()), 0755, true);
+    /**
+     * Get the class fully qualified name for the test.
+     */
+    protected function testClassFullyQualifiedName(): string
+    {
+        $module = $this->argument('module');
 
-    //     $result = File::put($path = $this->getTestPath(), $contents);
+        $name = Str::of(Str::lower($this->getNameInput()))->replace('.'.$this->option('extension'), '');
 
-    //     $this->components->info(sprintf('%s [%s] created successfully.', 'Test', $path));
+        $namespacedName = Str::of(
+            (new Stringable($name))
+                ->replace('/', ' ')
+                ->explode(' ')
+                ->map(fn ($part) => (new Stringable($part))->ucfirst())
+                ->implode('\\')
+        )
+            ->replace(['-', '_'], ' ')
+            ->explode(' ')
+            ->map(fn ($part) => (new Stringable($part))->ucfirst())
+            ->implode('');
 
-    //     return $result !== false;
-    // }
+        return "Modules\\{$module}\\tests\\Feature\\View\\{$namespacedName}";
+    }
+
+    /**
+     * Create the matching test case if requested.
+     */
+    protected function handleTestCreation($path): bool
+    {
+        if (! $this->option('test') && ! $this->option('pest') && ! $this->option('phpunit')) {
+            return false;
+        }
+
+        $contents = preg_replace(
+            ['/\{{ namespace \}}/', '/\{{ class \}}/', '/\{{ name \}}/'],
+            [$this->testNamespace(), $this->testClassName(), $this->testViewName()],
+            File::get($this->getTestStub()),
+        );
+
+        File::ensureDirectoryExists(dirname($this->getTestPath()), 0755, true);
+
+        $result = File::put($path = $this->getTestPath(), $contents);
+
+        $this->components->info(sprintf('%s [%s] created successfully.', 'Test', $path));
+
+        return $result !== false;
+    }
 }
