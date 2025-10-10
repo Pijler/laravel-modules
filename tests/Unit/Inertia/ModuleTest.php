f50836fa323a8211982fab501394ee9eb310c9e4
João Pedro Lopes <joaopalopes24@gmail.com>
2025-10-10 19:02:16 -0300

    ci(github): adding github workflow (pipeline)

--- tests/Unit/Inertia/ModuleTest.php
+++ tests/Unit/Inertia/ModuleTest.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\File;
 use Modules\Exceptions\FilePathIsIncorrectException;
 use Modules\Exceptions\FilePathNotSpecifiedException;
 use Modules\Exceptions\ModuleNameNotFoundException;
